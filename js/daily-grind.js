1aefbfe5117f33e9b14b4fcfc9ab196517fafdad
only1finesse <72461240+only1finesse@users.noreply.github.com>
2021-01-20 14:12:37 -0800

    modified js for wednesday coffee special

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,4 +1,85 @@
-let myDate = new Date();
-    myDay = myDate.getDay();
+/*
+Day of the Week - day
+Coffee Name - name
+Coffee pic - pic
+Alt Tag - alt
+Coffee Description - desc
+Color for Coffee - color
+*/
 
-    alert("Hi, it's " + myDay + ". I'm in another file now!");
+
+let myDate = new Date();
+myDay = myDate.getDay();
+today = "";
+coffee = "";
+
+function coffeeTemplate(coffee) {
+    let myReturn = " ";
+
+
+    myReturn =
+        `
+        <p>
+        <img src="${coffee.pic}" alt="Our ${coffee.alt}!" id="coffee">
+        <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong
+            class="feature">${coffee.name}</strong>, ${coffee.desc}
+        </p>
+        `;
+
+
+    return myReturn;
+}
+
+switch (myDay) {
+    case 0:
+        today = "Sunday";
+        coffee = {
+            name: "Drip",
+            color: "brown",
+            pic: "images/drip.jpg",
+            alt: "A piccture of Drip coffee",
+            day: "Wednesday",
+            desc: `Some days you just need the Drip`
+
+        };
+        break;
+
+    case 1:
+        today = "Monday";
+        break;
+
+    case 2:
+        today = "Tuesday";
+        break;
+
+    case 3:
+        today = "Wednesay";
+        coffee = {
+            name: "Drip",
+            color: "brown",
+            pic: "images/drip.jpg",
+            alt: "A piccture of Drip coffee",
+            day: "Wednesday",
+            desc: `Some days you just need the Drip`
+
+        };
+        break;
+
+    case 4:
+        today = "Thursday";
+        break;
+
+    case 5:
+        today = "Friday";
+        break;
+
+    case 6:
+        today = "Saturday";
+        break;
+}
+
+console.log(coffee);
+
+document.getElementById("coffee-output").innerHTML = coffeeTemplate(coffee);
+
+alert("Hi, it's " + today + ". I'm in another file now!");
