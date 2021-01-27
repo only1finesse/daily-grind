33dc5b14640348d5f477e99e58ba3d1e527ee94d
only1finesse <72461240+only1finesse@users.noreply.github.com>
2021-01-27 12:49:22 -0800

    add js and html to test what the page will look like

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -12,6 +12,15 @@ let myDate = new Date();
 myDay = myDate.getDay();
 today = "";
 coffee = "";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString); 
+
+if(urlParams.has('day')){//use query string
+     myDay = urlParams.get('day');
+     myDay = parseInt(myDay);
+ }else{              //use date object
+     myDay = myDate.getDay();
+ }
 
 function coffeeTemplate(coffee) {
     let myReturn = " ";
@@ -37,7 +46,7 @@ switch (myDay) {
             name: "Drip",
             color: "brown",
             pic: "images/drip.jpg",
-            alt: "A piccture of Drip coffee",
+            alt: "A picture of Drip coffee",
             day: "Wednesday",
             desc: `Some days you just need the Drip`
 
@@ -46,10 +55,26 @@ switch (myDay) {
 
     case 1:
         today = "Monday";
+        coffee = {
+            name: "Caramel Latte",
+            color: "#d7a96e",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of Caramel Latte",
+            day: "Monday",
+            desc: `Enjoy the sweetness of the Caramel Latte`
+        };
         break;
 
     case 2:
         today = "Tuesday";
+        coffee = {
+            name: "Cold Brew",
+            color: "#54282c",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of Cold Brew coffee",
+            day: "Tuesday",
+            desc: `Start your Tuesday with the Cold Brew!`
+        };
         break;
 
     case 3:
@@ -81,5 +106,15 @@ switch (myDay) {
 console.log(coffee);
 
 document.getElementById("coffee-output").innerHTML = coffeeTemplate(coffee);
+document.getElementsByTagName("HTML")[0].style.backgroundColor = coffee.color;
 
-alert("Hi, it's " + today + ". I'm in another file now!");
+// document.querySelectorAll("strong")[0].style.color = coffee.color;
+// document.querySelectorAll("strong")[1].style.color = coffee.color;
+
+// // Select the first div
+// const strong = document.querySelector('.feature');
+
+// // Assign the warning class to the first div
+// strong.setAttribute('style', `color: ${coffee.color}` )
+
+//alert("Hi, it's " + today + ". I'm in another file now!");
